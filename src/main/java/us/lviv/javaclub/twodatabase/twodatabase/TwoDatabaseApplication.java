b9ff1a020e4269e0ecfe3fc46d83f7121d0fbbde
Ihor Banadiga <banadiga@gmail.com>
2023-11-05 22:42:19 +0200

    init app

---
+++ src/main/java/us/lviv/javaclub/twodatabase/twodatabase/TwoDatabaseApplication.java
@@ -0,0 +1,13 @@
+package us.lviv.javaclub.twodatabase.twodatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/lviv/javaclub/twodatabase/twodatabase/TwoDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package us.lviv.javaclub.twodatabase.twodatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
